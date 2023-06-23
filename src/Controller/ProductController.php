3cd44dee8cbbdc2df9475275cc00c322882c75d2
saidouibrahim <ibrahim@zone-tech.online>
2023-06-23 09:54:57 -0700

    creation d un controlleur produit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+
+    #initialiser:#
+
+    public function __construct(private readonly EntityManagerInterface $em){}
+    
+
+    #[Route('/boutique', name: 'show_index')]
+    public function index(): Response
+    {
+       $products = $this->em->getRepository(Product::class)->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
